27e22c7aaf0c85ac76230ba6ec71da5dac20c2a8
juntaekHong <wnsxor3489@gmail.com>
2020-06-03 14:52:47 +0900

    1. 진료정보 페이지 - 카드 조회 뷰 구현

--- src/component/office/OfficeList.js
+++ src/component/office/OfficeList.js
@@ -3,27 +3,6 @@ import { connect } from "react-redux";
 import { Card } from "antd";
 import { OfficeActions } from "../../store/actionCreator";
 
-// "result": [
-//     {
-//       "officeIndex": 15,
-//       "hpid": "A1100008",
-//       "officeName": "김똘띵 선생님 - 소아과",
-//       "createdAt": "2020-04-06T13:46:09.000Z",
-//       "updatedAt": "2020-04-06T14:03:51.000Z",
-//       "treatment": [
-//         {
-//           "treatmentIndex": 7,
-//           "officeIndex": 15,
-//           "treatmentName": "구토"
-//         },
-//         {
-//           "treatmentIndex": 6,
-//           "officeIndex": 15,
-//           "treatmentName": "열"
-//         }
-//       ]
-//     },
-
 const OfficeList = (props) => {
   useEffect(() => {
     OfficeActions.getOffice();
@@ -31,18 +10,26 @@ const OfficeList = (props) => {
 
   return (
     <div>
-      <Card
-        title="진료 정보"
-        extra={<a href="#">수정</a>}
-        style={{ width: 300 }}
-      >
-        <p>Card content</p>
-        <p>Card content</p>
-        <p>Card content</p>
-      </Card>
+      {props.officeList.length !== 0
+        ? props.officeList.map((item) => {
+            return (
+              <Card
+                title="진료 정보"
+                extra={<a href="#">수정</a>}
+                style={{ width: 300 }}
+              >
+                <p>{item.officeName}</p>
+                {item.treatment.map((treatment) => {
+                  return <p>{treatment.treatmentName}</p>;
+                })}
+              </Card>
+            );
+          })
+        : null}
     </div>
   );
 };
 export default connect((state) => ({
   hospital: state.signin.hospital,
+  officeList: state.office.officeList,
 }))(OfficeList);
